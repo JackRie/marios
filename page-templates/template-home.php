ddcf3920801921c1243cdd372f44bb999c579741
JackRie <riemersmajackie@gmail.com>
2020-10-21 16:05:27 -0400

    inital commit

---
+++ page-templates/template-home.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Template: Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+
+<div class="splash">
+      <h1 class="splash-h1">Marios Pizza</h1>
+      <p class="splash-p">
+        Since 1980 we have been voted best pizza in Holland, but our tasty food
+        options don't stop there! We offer delicious Italian and Mexican food
+        all under one roof. Stop in today to try our fresh ingredients and let
+        our friendly staff serve you!
+      </p>
+</div>
+
+<?php
+get_footer();
